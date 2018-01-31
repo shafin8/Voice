e29ecf77383d96a80b03cb34c0c832b351470550
shafin <oshafin@gmail.com>
2018-02-01 01:23:27 +0600

    file created

---
+++ Voice/Voice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voice
+//
+//  Created by shafin haq on 1/2/18.
+//  Copyright © 2018 shafin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
